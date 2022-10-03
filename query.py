aaebaa374bde5371219917a58c82f9a7fa883b32
Michael Martis <michaeljmartis@gmail.com>
2022-10-04 01:42:15 +1100

    Added selection logic

--- query.py
+++ query.py
@@ -1,7 +1,7 @@
 # Finds the closest image in the given index.
 #   Usage: query.py <index file> <image file>
 
-from datetime import datetime
+import datetime
 import pickle
 import sys
 
@@ -17,13 +17,27 @@ MODEL_DIR = 'imagenet_mobilenet_v2_140_224_feature_vector'
 MODEL_INPUT_DIMS = (224, 224)
 
 BATCH_SIZE = 32
-CANDIDATES_SIZE = 5
 
-# Print out the "min:sec" formatted version of the given
+CANDIDATES_SIZE = 5
+CLOSE_TIME = datetime.timedelta(seconds=5.5)
+MAX_DISTANCE = 0.15
+
+# Returns the "min:sec" formatted version of the given
 # second duration.
 def format_duration(s):
     mins, secs = divmod(s, 60)
     return f'{mins}:{secs:02}'
+
+# Returns a string representing the given datapoint.
+def format_datapoint(d, include_ts):
+    date_str = d.date.strftime('%-m %b %Y')
+
+    if include_ts:
+        length_str = format_duration(d.length.seconds)
+        timestamp_str = format_duration(d.timestamp.seconds)
+        return f'"{d.title}" ({date_str}) [{timestamp_str}/{length_str}]'
+
+    return f'"{d.title}" ({date_str})'
 
 def main():
     if len(sys.argv) != 3:
@@ -68,19 +82,41 @@ def main():
                                         'cosine')[0]
 
             # Calculate the closest n from this batch.
-            part = np.argpartition(ds, CANDIDATES_SIZE)[:-CANDIDATES_SIZE]
-            batch_closest = [(ds[i], batch[i]) for i in part]
+            if len(batch) > CANDIDATES_SIZE:
+                part = np.argpartition(ds, CANDIDATES_SIZE)[:-CANDIDATES_SIZE]
+                batch_closest = [(ds[i], batch[i]) for i in part]
 
             # Merge this batch with the closest seen so far.
             closest = sorted(closest + batch_closest)[:CANDIDATES_SIZE]
 
             head = database.peek(1)
 
-    for _, c in closest[:CANDIDATES_SIZE]:
-        date_str = c.date.strftime('%-m %b %Y')
-        length_str = format_duration(c.length.seconds)
-        timestamp_str = format_duration(c.timestamp.seconds)
-        print(f'"{c.title}" ({date_str}) [{timestamp_str}/{length_str}]')
+    ## Debug output.
+    #for d, c in closest:
+    #    print(f'{format_datapoint(c, True)} <d {d}>.')
+
+    # Our closest datapoints in chronological order.
+    candidates = sorted([c for _,c in closest], key=lambda c: c.timestamp)
+
+    # All results must be from the same video.
+    if any(c.title != candidates[0].title for c in candidates):
+        print('Not found.')
+        exit()
+
+    chosen_dist, chosen = closest[0]
+
+    # If most of the candidates are far away from our selection
+    # chronologically, then don't offer timestamp.
+    half_count = len(candidates) // 2
+    far = lambda c1, c2: abs(c1.timestamp - c2.timestamp) > CLOSE_TIME
+    timestamp_bad = sum(far(c, chosen) for c in candidates) > half_count
+
+    # Selection must be moderately close.
+    if chosen_dist > MAX_DISTANCE:
+        print('Not found.')
+        exit()
+
+    print(format_datapoint(chosen, not timestamp_bad))
 
 if __name__ == "__main__":
     main()
